652be4ac92fba254457dc0449edf158ec9088758
Takexito <Takexito@gmail.com>
2016-07-13 16:33:45 +0300

    фикс анимации

--- Assets/Scripts/PikCrack.cs
+++ Assets/Scripts/PikCrack.cs
@@ -18,8 +18,8 @@ public class PikCrack : MonoBehaviour {
 		if (coll.gameObject.tag == "Player") {
 			posx = GetComponent<Transform> ().position.x;
 			posy = GetComponent<Transform> ().position.y;
-			GameObject.Find("Pik").GetComponent<Animation>().Play();
-			Destroy (this.gameObject);
+			this.GetComponent<Animation>().Play();
+            this.GetComponent<Collider2D>().enabled = false;
 		}
 	}
 }
