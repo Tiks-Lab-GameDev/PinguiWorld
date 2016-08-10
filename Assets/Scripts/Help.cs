1ebe9b1fff8505b4d9d5ff62837b8a5f3f1d03b0
Takexito <Takexito@gmail.com>
2016-08-10 23:49:28 +0300

    хелп

---
+++ Assets/Scripts/Help.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Help : MonoBehaviour {
+
+    public GameObject go;
+    bool active;
+	// Use this for initialization
+	void Start () {
+        GameObject.Find("Toggle").GetComponent<Toggle>().isOn = bool.Parse(PlayerPrefs.GetString("Active"));
+        if (bool.Parse(PlayerPrefs.GetString("Active"))) Destroy(go);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void ToggleActive()
+    {
+        active = !active;
+        PlayerPrefs.SetString("Active", active.ToString());
+    }
+
+    public void Close()
+    {
+        Destroy(go);
+    }
+}
